fb1a620d68c907a43e260b5d7ad53167e6269617
ghaidaashtayeh <ghaidaashtayeh@hotmail.com>
2022-07-22 11:28:00 +0300

    update thefts

--- src/main/java/sart/carsThefts.java
+++ src/main/java/sart/carsThefts.java
@@ -18,19 +18,19 @@ public class carsThefts {
                 .master("local")
                 .appName("cars")
                 .config("spark.some.config.option", "some-value")
-                .config("spark.sql.crossJoin.enabled", "true")
                 .getOrCreate();
 
-
+    ////////////////////////////////////////////////////////////////////////
         // Read csv files locally
         Dataset<Row> originalTheftsTable = spark.read().option("delimiter", ",").option("header", "true").csv(theftsPath).cache();
         Dataset<Row> originalCars = spark.read().option("delimiter", ",").option("header", "true").csv(carPath).cache();
         Dataset<Row> updates = spark.read().option("delimiter", ",").option("header", "true").csv(updatePath).cache();
 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         // DataSets pre-processing
         Dataset<Row> modelTable = originalTheftsTable.withColumnRenamed("Make/Model", "Car_Model").withColumnRenamed("Model Year", "Year").cache();
-        Dataset<Row> modelTheftsTable = modelTable.select("Car_Model", "Year", "Thefts");
+        Dataset<Row> modelTheftsTable = modelTable.select("Car_Model", "Year", "Thefts" , "State");
 
         System.out.print("Thefts Table Data : \n");
         modelTheftsTable.show(5);
@@ -50,33 +50,33 @@ public class carsThefts {
         long count = updatedCarTable.count();
         System.out.print(count + " :  after || before : " + originalTheftsTable.count() + "\n");
 
-        // the results show that are no nulls values in the table
-
+        //////////////////////////////////////////////////////////////////////////////////////
         //Partition based in car model column :
         Dataset<Row> carsTheftsFinal = updatedCarTable.repartition(functions.col("Year")).withColumnRenamed("Country of Origin", "Origin").cache();
         System.out.print("repartition \n");
-        carsTheftsFinal.show(500, false);
-        /////////////////////////////////////////////////////////////////////////////////////
-        /* merge these updates with the original dataset */
-        /*   updates.createOrReplaceTempView("updatesTable");
-        originalTheftsTable.createOrReplaceTempView("originalTheftsTable");
-        Dataset<Row> test = spark.sql( "SELECT * FROM updatesTable JOIN originalTheftsTable ON updatesTable.State = originalTheftsTable.State AND updatesTable.Make/Model = originalTheftsTable.Make/Model AND z1 = z2");
-*/
-        ////////////////////////////////////////////////////////////////////////////////////
-        System.out.print(carsTheftsFinal.count());
+        carsTheftsFinal.show(10);
 
-        Dataset<Row> updatesNew = updates.withColumnRenamed("Model Year", "Year");
-        updatesNew.createOrReplaceTempView("updatesTable");
-        carsTheftsFinal.createOrReplaceTempView("carsTheftsFinal2");
-        updatesNew.show();
-        //  Dataset<Row> test = spark.sql( "ALTER carsTheftsFinal2 set carsTheftsFinal2.Thefts = updatesTable.Thefts from updatesTable where carsTheftsFinal2.Car_Model = updatesTable.Make/Model AND carsTheftsFinal2.State = updatesTable.State AND carsTheftsFinal2.Year = updatesTable.Year ");
-        //  test.show(12);
+        ////////////////////////////////////////////////////////////////////////////////////
+
+        Dataset<Row> updatesNew = updates.withColumnRenamed("Model Year", "Year2").withColumnRenamed("Thefts", "Thefts2").withColumnRenamed("Make/Model" , "Car_Model2").withColumnRenamed("State" , "State2").cache();
+
+
+        Dataset<Row> updatesNe2 = carsTheftsFinal.join(updatesNew, carsTheftsFinal.col("State").equalTo(updatesNew.col("State2")).and(carsTheftsFinal.col("Year").equalTo(updatesNew.col("Year2")))
+                        .and(carsTheftsFinal.col("Car_Model").equalTo(updatesNew.col("Car_Model2"))),"left")
+                .withColumn("Thefts",
+                        functions.when(functions.col("Thefts2").isNotNull(), functions.col("Thefts2")).otherwise(functions.col("Thefts"))
+                )
+                // finally, we drop duplicated
+                .drop("Thefts2","State2" , "Car_Model2" , "Year2" , "Rank").cache();
+
+        System.out.print("updated");
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
 
         //most 5 countries from where Americans buy their thefted cars
         Dataset<Row> topThefts = carsTheftsFinal.select("Origin", "Thefts").groupBy("Origin").agg(functions.sum(functions.col("Thefts"))).cache();
         Dataset<Row> csvFile = topThefts.orderBy(functions.col("sum(Thefts)").desc()).cache();
-        // csvFile.write().format("com.databricks.spark.csv").option("inferSchema", "false").option("header", true)
-        //        .option("charset", "UTF-8").option("delimiter", ",").option("quote", "\'").mode(SaveMode.Overwrite).save("src/main/java/sart/data");
+
         csvFile.show(5);
     }
 }
